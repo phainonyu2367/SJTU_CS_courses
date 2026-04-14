885cca10a89819d031a5f76a22402c77142739f5
phainon2367 <robertyu2024@163.com>
2026-04-14 13:17:50 +0800

    [feat]: updated solution for problem 3062.

--- SJTU_CS0501H/code/lec 7/delete_adjacent_duplicate_members/delete_adjacent_duplicate_members.cpp
+++ SJTU_CS0501H/code/lec 7/delete_adjacent_duplicate_members/delete_adjacent_duplicate_members.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <string>
 
+std::string removeReplicate(const std::string& str) {
+    bool modified = false;
+    std::string result = str;
 
+    do {
+    modified = false;
+    for (int i = 0; i < result.size() - 1; ++i) {
+        if (result[i] == result[i + 1]) {
+            modified = true;
+            result.erase(i, 1); 
+            result.erase(i, 1);
+            i--;
+        }
+
+        if (result.empty()) {
+            return "";
+        } 
+    }
+    } while (modified);
+    return result;
+}
 
 int main() {
+    std::string str;
+    std::cin >> str;
+    std::cout << removeReplicate(str);
     return 0;
 }
